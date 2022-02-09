89ee5b716c4c4183e77d5ef49767fe089a600795
korei999 <93387739+korei999@users.noreply.github.com>
2022-02-09 21:16:18 +0200

    Add files via upload

---
+++ home/dwm/autostart.sh
@@ -0,0 +1,10 @@
+dwmblocks &
+blueman-applet &
+picom --experimental-backends &
+nitrogen --restore &
+/usr/bin/pipewire &
+/usr/bin/pipewire-pulse &
+/usr/bin/pipewire-media-session &
+mpris-proxy &
+xbindkeys &
+/usr/bin/dunst &

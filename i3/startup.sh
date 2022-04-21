6c543a9fd5919ef713d94b4113b012b6e4418f0d
korei999 <93387739+korei999@users.noreply.github.com>
2022-04-21 22:27:31 +0300

    Add files via upload

---
+++ i3/startup.sh
@@ -0,0 +1,21 @@
+killall -q pipewire
+killall -q pipewire-pulse
+killall -q mpris-proxy
+killall -q wireplumber
+killall -q polkit-gnome-authentication-agent-1
+killall -q nm-applet
+
+xset r rate 200 50 &
+setxkbmap -layout us,ru,ua &
+~/.config/polybar/launch.sh &
+nvidia-settings --load-config-only &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+nm-applet &
+blueman-applet &
+picom &
+nitrogen --restore &
+/usr/bin/pipewire &
+/usr/bin/pipewire-pulse &
+/usr/bin/wireplumber &
+mpris-proxy &
+/usr/bin/dunst &

b5179cb148b96f4b1668f7e6ee89e6575821f523
korei999 <93387739+korei999@users.noreply.github.com>
2022-04-30 02:45:41 +0300

    Add files via upload

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,15 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    {"", "pulseaudio-control --icons-volume \" , \" --icon-muted \" \" --sink-nicknames-from \"device.description\" --sink-nickname \"alsa_output.usb-Focusrite_Scarlett_Solo_USB-00.analog-stereo: Speakers\" --sink-nickname \"alsa_output.pci-0000_00_1f.3.analog-stereo: Mobo\" listen &",     0,             12},
+
 	{"CPU:", "sensors | awk '/^Package id 0/ { print $4}' | sed s/i//g",     5,             0},
 
-	{"GPU:+", "nvidia-settings -q gpucoretemp -t | sed 2d",    		 5,             0},
+	{"GPU:+", "nvidia-settings -q gpucoretemp -t | sed 2d",    		         5,             0},
 
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	    30,     		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "date '+%b %d (%a) %I:%M%p'",				                    	 5,	        	0},
 
-	{"", "xkblayout-state print %e",                                      0,              11}
+	{"", "xkblayout-state print %e",                                         0,             11}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

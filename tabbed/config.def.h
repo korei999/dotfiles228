5dcef6ec73241d2dc1614465a074ca850c88da6d
korei999 <93387739+korei999@users.noreply.github.com>
2022-05-02 18:09:37 +0300

    Add files via upload

--- tabbed/config.def.h
+++ tabbed/config.def.h
@@ -5,7 +5,7 @@ static const char font[]        = "JetBrains Mono NL:size=12";
 static const char* normbgcolor  = "#222222";
 static const char* normfgcolor  = "#cccccc";
 static const char* selbgcolor   = "#555555";
-static const char* selfgcolor   = "#ffffff";
+static const char* selfgcolor   = "#dfdfdf";
 static const char* urgbgcolor   = "#111111";
 static const char* urgfgcolor   = "#cc0000";
 static const char before[]      = "<";
@@ -40,9 +40,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
 
 	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_j,      rotate,      { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_i,      movetab,     { .i = +1 } },
 	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },

a75d40160d3972e29e7faf5959738ee1b025033f
korei999 <93387739+korei999@users.noreply.github.com>
2022-05-03 03:06:18 +0300

    Add files via upload

--- tabbed/config.def.h
+++ tabbed/config.def.h
@@ -43,6 +43,12 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,     XK_j,      rotate,      { .i = -1 } },
 	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = -1 } },
 	{ MODKEY|ShiftMask,     XK_i,      movetab,     { .i = +1 } },
+
+	{ MODKEY|ShiftMask,     XK_Right,  rotate,      { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_Left,   rotate,      { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_Down,   movetab,     { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_Up,     movetab,     { .i = +1 } },
+
 	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
@@ -57,7 +63,7 @@ static Key keys[] = {
 	{ MODKEY,               XK_9,      move,        { .i = 8 } },
 	{ MODKEY,               XK_0,      move,        { .i = 9 } },
 
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
+	{ MODKEY|ShiftMask,     XK_w,      killclient,  { 0 } },
 
 	{ MODKEY,               XK_u,      focusurgent, { 0 } },
 	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
